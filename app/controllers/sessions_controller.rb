e2cb4be7ef2d4837b7a56f74b454ba17013775d1
Kyle Smith <tksmith@crimson.ua.edu>
2011-06-23 00:16:09 -0400

    Logged in username appears in the header and links to the details of the user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
 	if user = User.authenticate(params[:name], params[:password])
 		session[:user_id] = user.id
+		session[:user_name] = user.name
 		redirect_to admin_url
 	else
 		redirect_to login_url, :alert => "Invalid user/password combination"
